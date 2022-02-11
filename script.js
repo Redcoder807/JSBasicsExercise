82e35b491cdc84cb101d50c9ee9cc8ffe76ed651
Redcoder807 <gearonhaynes@gmail.com>
2022-02-11 14:11:51 -0600

    All but 5

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log(64);
+// Numbers, strings, booleans, null, and undefined
+// + - * / ** %
+
+console.log(5%4);
+console.log(5%2);
+console.log('I\'M MARRY POPPINS , Y\'ALL');
+console.log("Hawk" + "eye");
+console.log("Gearon".length + "Haynes".length);
+console.log("Avengers: Endgame"[16]);
+
+// Bonus
+
+console.log("\\");
+console.log(5** 4- 9);
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURRY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
