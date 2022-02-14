a2b861e24b8e9938e7dc0b1870b3016c84a36c70
Redcoder807 <gearonhaynes@gmail.com>
2022-02-14 10:41:06 -0600

    Finished Bonus

--- script.js
+++ script.js
@@ -3,17 +3,25 @@ console.log(64);
 // + - * / ** %
 
 console.log(5%4);
+
 console.log(5%2);
+
 console.log('I\'M MARRY POPPINS , Y\'ALL');
+
 console.log("Hawk" + "eye");
+
 console.log("Gearon".length + "Haynes".length);
+
 console.log("Avengers: Endgame"[16]);
 
 // Bonus
 
 console.log("\\");
+
 console.log(5** 4- 9);
+
 console.log("I".length - "Am".length + "Iron".length - "Man".length);
+
 console.log("CAP"[1]);
 console.log("VISION"[0]);
 console.log("OKOYE"[4]);
@@ -22,3 +30,8 @@ console.log("GROOT"[0]);
 console.log("DOCTOR STRANGE"[13]);
 console.log("THOR"[3]);
 console.log("STAR-LORD"[0]);
+
+m = 25;
+c =4;
+u = m + c;
+console.log(u);
